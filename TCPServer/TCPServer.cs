23602d3e2d9bbb9b491290dc1a232f16ae81413a
Panzerfahrer1 <oliverangerer0@gmail.com>
2025-12-23 07:42:59 +0100

    Added a Start function to TCPServer class

---
+++ TCPServer/TCPServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPServer
+{
+    internal class TCPServer
+    {
+        private TcpListener server;
+
+        public TCPServer(int port, IPAddress serverAdress)
+        {
+            server = new TcpListener(serverAdress, port);
+        }
+
+        public void Start()
+        {
+            try
+            {
+                server.Start();
+            }
+            catch (SocketException ex)
+            {
+                throw new Exception("Could not start server", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unexpected error occurred while starting the server", ex);
+            }
+        }
+    }
+}
