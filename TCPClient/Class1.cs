5087a953fe3d95759ad489e402933a151ee78f40
Panzerfahrer1 <oliverangerer0@gmail.com>
2025-12-23 08:40:10 +0100

    Final commit

---
+++ TCPClient/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    internal class tcpClient
+    {
+        TcpClient client;
+        NetworkStream stream;
+
+        public tcpClient(int port, IPAddress address)
+        {
+            client = new TcpClient(address.ToString(), port);
+            stream = client.GetStream();
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            stream.Write(data, 0, data.Length);
+        }
+
+        public string GetMessage()
+        {
+            byte[] buffer = new byte[1024];
+
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if(bytesRead == 0)
+            {
+                return string.Empty;
+            }
+            return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        }
+    }
+}
---
+++ TCPClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using TCPClient;
+
+tcpClient client = new tcpClient(8080, System.Net.IPAddress.Loopback);
+client.SendMessage("Hello from TCP Client!");
+Console.WriteLine(client.GetMessage()); 
+System.Threading.Thread.Sleep(1000);
+client.SendMessage("Goodbye from TCP Client!");
+Console.WriteLine(client.GetMessage());
+client.SendMessage("Goodbye from TCP Client!");
+Console.WriteLine(client.GetMessage());
+client.SendMessage("Goodbye from TCP Client!");
+Console.WriteLine(client.GetMessage());
+client.SendMessage("Goodbye from TCP Client!");
+client.SendMessage("Goodbye from TCP Client!");
+Console.WriteLine(client.GetMessage());
+Console.WriteLine(client.GetMessage());
